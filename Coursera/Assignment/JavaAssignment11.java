97e3ba8e9f623fcd2df6c6517002f7d06b8252ab
vamshiraghav <vamshiraghava@gmail.com>
2020-05-01 09:48:50 +0530

    Java Assignment -character count to file

--- Coursera/Assignment/JavaAssignment11.java
+++ Coursera/Assignment/JavaAssignment11.java
@@ -22,49 +22,47 @@ public class JavaAssignment11 {
         Scanner scan = new Scanner(System.in);
         String fName = scan.nextLine();
         scan.close();
-            File file = new File(path + fName);
-            System.out.println(path + fName);
-            Map<Character, Integer> fileMap = new HashMap<>();
-            char temp;
-            if (!file.isDirectory()) {
-                FileReader reader = new FileReader(file.toString());
-                int character;
-                while ((character = reader.read()) != -1) {
-                    temp = (char) character;
-                    if(fileMap.containsKey(temp)) {
-                        fileMap.put(temp,fileMap.get(temp)+1);
-                    }else{
-                        fileMap.put(temp,1);
+        File file = new File(path + fName);
+        System.out.println(path + fName);
+        Map<Character, Integer> fileMap = new HashMap<>();
+        char temp;
+        if (!file.isDirectory()) {
+            FileReader reader = new FileReader(file.toString());
+            int character;
+            while ((character = reader.read()) != -1) {
+                temp = (char) character;
+                if (fileMap.containsKey(temp)) {
+                    fileMap.put(temp, fileMap.get(temp) + 1);
+                } else {
+                    fileMap.put(temp, 1);
 
-                    }
                 }
-                reader.close();
-                System.out.println("file read..");
-                FileWriter writer = new FileWriter(path+"MyFile.txt", false);
-                BufferedWriter bufferedWriter = new BufferedWriter(writer);
-                for(char c:fileMap.keySet()){
-                    bufferedWriter.write(c +" = "+fileMap.get(c));
-                    bufferedWriter.newLine();
-                }
-                bufferedWriter.close();
-                System.out.println("file written");
-
-
             }
-            else {
-                System.out.println("Enter the correct file name..");
+            reader.close();
+            System.out.println("file read..");
+            FileWriter writer = new FileWriter(path + "MyFile.txt", false);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            for (char c : fileMap.keySet()) {
+                bufferedWriter.write(c + " = " + fileMap.get(c));
+                bufferedWriter.newLine();
             }
+            bufferedWriter.close();
+            System.out.println("file written");
 
+
+        } else {
+            System.out.println("Enter the correct file name..");
+        }
 
 
     }
 
     public static void main(String[] args) {
         JavaAssignment11 object = new JavaAssignment11();
-        String path="./Assignment/";
+        String path = "./Assignment/";
         try {
             object.charCountToFile(path);
-        }catch (Exception e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
